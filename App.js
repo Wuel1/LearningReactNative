bbfbe029417b0f8af003f2c98d2a683864bc5b77
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2023-11-22 11:19:30 -0300

    0.0.1 | Commit Inicial

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Olá mundo! Voltei s </Text>
+      <Text> Testando </Text>
       <StatusBar style="auto" />
     </View>
   );
