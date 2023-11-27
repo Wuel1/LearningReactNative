642f9060b7c401e45a766a7837ae28ad55188420
Wandson Emanuel <107449320+Wuel1@users.noreply.github.com>
2023-11-27 12:25:51 -0300

    style do title

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Title from './scr/components/Title/Index';
 import Main from './scr/components/Main/Index';
+import Form from './scr/components/Form/Index';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
       <Title/>
-      <Main/>
+      <Form/>
     </View>
   );
 }
@@ -16,8 +17,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#16041B',
+    paddingTop: 80,
   },
 });
